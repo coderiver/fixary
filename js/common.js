1bab17d416ec44cd1b875501bc8d31d2b16239bc
andreyka_g <andreyka_g@ukr.net>
2015-05-09 04:12:54 +0300

    partly sass without footer

--- js/common.js
+++ js/common.js
@@ -1,4 +1,12 @@
 head.ready(function() {
 
+	$('.greet__form_item').change(function(){
+		var $this = $(this);
+		if($this.val().length) {
+			$this.addClass('filled');
+		} else {
+			$this.removeClass('filled');
+		}
+	});
 
 });
