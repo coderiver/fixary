5a202e7b65deb41e64b559520f86f6018e712468
akella <akella.a@gmail.com>
2015-05-12 01:47:15 +0300

    fixes

--- js/common.js
+++ js/common.js
@@ -9,7 +9,41 @@ head.ready(function() {
 		}
 	});
 	$('.js-nav-toggle').click(function(){
-			$('.out').toggleClass('trans');
+			$('body').toggleClass('menu');
 	});
 
+	$(window).scroll(function(event) {
+		st = $(window).scrollTop();
+		$header = $('.header');
+		if(st>20){
+			$header.addClass('is-atbottom');
+		}
+		else{
+			$header.removeClass('is-atbottom');
+		}
+	});
+
+	$('.dropdown,dropdown__menu').click(function(event) {
+		$(this).toggleClass('is-active');
+	});
+
+	function visibility(){
+			var window_top = $(window).scrollTop();
+			var window_height = $(window).height();
+			var start_visibility = window_top + window_height;
+			
+			$(".js-view").each(function(){
+				
+				var block_position = $(this).offset().top;
+				
+				if(start_visibility >= block_position){
+					$(this).addClass('animated');
+				}
+			});
+		}
+		visibility();
+		$(window).scroll(function(){
+			visibility();
+		});
 });
+
